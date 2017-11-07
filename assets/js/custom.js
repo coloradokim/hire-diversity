1e4d61533d8d3926b6584cdba0adc4bf69eee632
coloradokim <schlesinger.kim@gmail.com>
2017-11-07 13:13:31 -0700

    add new branch

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    document.getElementById("big-btn").onclick = function() {
+        alert("hello");
+    }
+}
+
+
+
+
+
+// function submitForm() {
+//   let button = document.getElementById('big-btn')
+//   button.oncclick = function() {
+//     alert("hello");
+//     return false;
+//   }
+// };
+//
+
+// document.getElementByClassName("icon").addEventListener("click", function(event) {
+// event.preventDefault();
+// }, false);
